e191c0bf5ce1ea6a7d2c4b3efe99a9e165b2ca54
ycchen-tw <88210435+ycchen-tw@users.noreply.github.com>
2021-07-31 03:32:31 +0800

    First Commit

--- quick_train_guide.sh
+++
@@ -1,6 +0,0 @@
-# export NCCL_P2P_DISABLE=1
-export NGPUS=4
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$NGPUS tools/train_net.py \
-	--config-file "configs/r50_baseline.yaml" \
-	--skip-test
-
--- single_image_demo.sh
+++
@@ -1,8 +0,0 @@
-DIR=/path/to/JPG_IMAGE_DIR
-ODIR=/path/to/OUTPUT_DIR/
-CUDA_VISIBLE_DEVICES=0 python demo/test_single_image.py \
-	--min-image-size 1000 \
-	--config-file configs/r50_baseline.yaml \
-	--output_dir  $ODIR\
-	--img $DIR
-	
